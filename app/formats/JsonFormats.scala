a9d7832e6a1478b1394bf2cae7aa22d10d2fc274
Julien DAUPHANT <judauphant@gmail.com>
2015-07-17 11:29:32 +0200

    Add ObjectId to String formats

--- app/formats/JsonFormats.scala
+++ app/formats/JsonFormats.scala
@@ -14,8 +14,18 @@ object JsonFormats {
       js.as[JsObject] - "_type"  ++ Json.obj("type" ->  (js \ "_type").get)
   }
 
-  def typeReads[T](r: Reads[T]) = JsPath.json.update((JsPath \ '_type).json.copyFrom((JsPath \ 'type).json.pick[JsString])) andThen r
+  def mongoWritesObjectId[T](w: Writes[T]) = OWrites[T] {
+    a: T =>
+      val js = w.writes(a)
+      js.as[JsObject] - "id" ++ Json.obj("_id" -> Json.obj("$oid" -> (js \ "id").get))
+  }
 
-  implicit val feedRead = typeReads[User](Json.reads[User])
-  implicit val userWrite = typeWrites[User](Json.writes[User])
+  def typeReads[T](r: Reads[T]) =
+    JsPath.json.update((JsPath \ '_type).json.copyFrom((JsPath \ 'type).json.pick[JsString])) andThen r
+
+  def mongoReadsObjectId[T](r: Reads[T]) =
+    JsPath.json.update((__ \ 'id).json.copyFrom((JsPath \ '_id \ '$oid).json.pick[JsString] )) andThen r
+
+  implicit val feedRead = typeReads[User](mongoReadsObjectId[User](Json.reads[User]))
+  implicit val userWrite = typeWrites[User](mongoWritesObjectId[User](Json.writes[User]))
 }
